481a56501383136a7a6a33f4775d82a28badaf78
Guangyuan Piao <parklize@gmail.com>
2024-11-05 14:19:29 +0000

    update removing LIMIT

--- datasets/send_data.py
+++ datasets/send_data.py
@@ -117,22 +117,22 @@ def main(
                 #jf.pop('image_base64')
                 #print(f'{jf}')
                 expanded = expand_json(jf, subdir)
-                expanded.pop('image_base64')
+                #expanded.pop('image_base64')
                 print(json.dumps(expanded, indent=2))
                 json_data = json.dumps(expanded)
     
                 # Send the JSON data to the Apache NiFi endpoint
-                #response = requests.post(
-                #    nifi_url, 
-                #    data=json_data, 
-                #    headers={"Content-Type": "application/json"}
-                #)
-                #
-                ## Check if the request was successful
-                #if response.status_code == 200:
-                #    print(f"Successfully sent {filename} from {subdir} to Apache NiFi.")
-                #else:
-                #    print(f"Failed to send {filename} from {subdir}. Status code: {response.status_code}")
+                response = requests.post(
+                    nifi_url, 
+                    data=json_data, 
+                    headers={"Content-Type": "application/json"}
+                )
+                
+                # Check if the request was successful
+                if response.status_code == 200:
+                    print(f"Successfully sent {filename} from {subdir} to Apache NiFi.")
+                else:
+                    print(f"Failed to send {filename} from {subdir}. Status code: {response.status_code}")
                 
             if test_mode:
                 break
@@ -142,6 +142,6 @@ def main(
 
 if __name__ == '__main__':
     main(
-        test_mode=True,
+        test_mode=False,
         nifi_url="http://semantification.integration/contentListener"
     )
